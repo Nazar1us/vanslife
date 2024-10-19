1d9639310a1a734ed4027337001877e4d0ea41aa
Nazar1us <35458441+Nazar1us@users.noreply.github.com>
2024-10-19 12:48:40 -0400

    first commit

---
+++ api.js
@@ -0,0 +1,86 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import {
+  getDocs,
+  getFirestore,
+  collection,
+  doc,
+  getDoc,
+  query,
+  where,
+} from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDjmw1CnC4_r8QYe7UmoDfmajKl0s1nMQ8",
+  authDomain: "vanlife-7f0b7.firebaseapp.com",
+  projectId: "vanlife-7f0b7",
+  storageBucket: "vanlife-7f0b7.appspot.com",
+  messagingSenderId: "66115793646",
+  appId: "1:66115793646:web:4e9a042cfc0acf66f839ff",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const vansCollectionRef = collection(db, "vans");
+
+export async function getVans() {
+  const snapshot = await getDocs(vansCollectionRef);
+  const vans = snapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+  return vans;
+}
+
+export async function getVan(id) {
+  const docRef = doc(db, "vans", id);
+  const snapshot = await getDoc(docRef);
+  return {
+    ...snapshot.data(),
+    id: snapshot.id,
+  };
+}
+
+export async function getHostVans() {
+  const q = query(vansCollectionRef, where("hostId", "==", "123"));
+  const snapshot = await getDocs(q);
+  const vans = snapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+  return vans;
+}
+
+// export async function getHostVans(id) {
+//   const url = id ? `/api/host/vans/${id}` : "/api/host/vans";
+//   const res = await fetch(url);
+//   if (!res.ok) {
+//     throw {
+//       message: "Failed to fetch vans",
+//       statusText: res.statusText,
+//       status: res.status,
+//     };
+//   }
+//   const data = await res.json();
+//   return data.vans;
+// }
+
+export async function loginUser(creds) {
+  const res = await fetch("/api/login", {
+    method: "post",
+    body: JSON.stringify(creds),
+  });
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw {
+      message: data.message,
+      statusText: res.statusText,
+      status: res.status,
+    };
+  }
+
+  return data;
+}
